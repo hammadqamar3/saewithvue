72ffbd2d9475c8b4840b99663c98253df326b4b0
anas40 <anasbeg50@gmail.com>
2020-04-02 04:05:30 +0530

    non required packages removed

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+  
+
 module.exports = {
     configureWebpack: {
       externals: {
         Vue: "vue"
-      }
+      },
+      // plugins: [
+      //   new BundleAnalyzerPlugin()
+      //   ]
     }
   };
