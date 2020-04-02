1161a34b4b2a701ba2a6b89e38ae0d9947dd3985
anas40 <anasbeg50@gmail.com>
2020-04-02 21:32:42 +0530

    404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Page not Found</h1>
+    <p>
+      try going to
+      <router-link :to="{name:'Home'}">Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+    div{
+        margin: 100px 0;
+    }
+    h1,p{
+        text-align: center;
+        margin: auto;
+    }
+</style>
